18b67b38da6afb2cfc6155a917640c605da1d67c
dtlucho <luciano.ditomaso@gmail.com>
2025-02-02 22:09:08 -0300

    create groups

--- main.py
+++ main.py
@@ -11,7 +11,14 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
 
 
@@ -19,12 +26,17 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        updatable.update(dt)
         
         screen.fill((0, 0, 0))
-        player.draw(screen) # draw the player
-        player.update(dt) # update the player
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 
